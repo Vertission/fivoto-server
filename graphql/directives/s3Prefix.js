7d7c4c378bf4ed9cb9fa61b33048b17078c0db27
usama <usamaasfar.mua@gmail.com>
2021-03-10 09:41:43 +0530

    fix return type for string

--- graphql/directives/s3Prefix.js
+++ graphql/directives/s3Prefix.js
@@ -1,14 +1,27 @@
 const { SchemaDirectiveVisitor } = require('apollo-server');
-const { defaultFieldResolver } = require('graphql');
+const { defaultFieldResolver, GraphQLString, GraphQLList } = require('graphql');
 
 class S3Prefix extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     const { resolve = defaultFieldResolver } = field;
     field.resolve = async function (...args) {
       const result = await resolve.apply(this, args);
-      if (!result) return [];
-      else if (typeof result === 'string') return [process.env.AWS_S3_PREFIX, result].join('');
-      else return result.map((key) => [process.env.AWS_S3_PREFIX, key].join(''));
+
+      if (field.type === GraphQLString) {
+        if (result) {
+          return [process.env.AWS_S3_PREFIX, result].join('');
+        } else {
+          return null;
+        }
+      }
+
+      if (field.type === GraphQLList) {
+        if (result) {
+          return result.map((key) => [process.env.AWS_S3_PREFIX, key].join(''));
+        } else {
+          return [];
+        }
+      }
     };
   }
 }
